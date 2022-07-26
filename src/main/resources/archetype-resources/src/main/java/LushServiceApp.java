aaa3da3986e5770a8a4b92221f98bd5ed1e783df
Paul Parrone <paul.parrone@gmail.com>
2022-07-26 18:57:44 -0400

    Added OpenAPI
    Took 3 minutes

--- src/main/resources/archetype-resources/src/main/java/LushServiceApp.java
+++ src/main/resources/archetype-resources/src/main/java/LushServiceApp.java
@@ -1,6 +1,8 @@
 package ${package};
 
+import io.swagger.v3.oas.models.parameters.Parameter;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,4 +23,16 @@ public class LushServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(LushServiceApp.class, args);
     }
+
+    @Bean
+    public OperationCustomizer customize() {
+        return (operation, handlerMethod) -> operation.addParametersItem(
+                new Parameter()
+                        .in("header")
+                        .required(true)
+                        .description("Lush Ticket as JSON")
+                        .name("x-lush-who")
+        );
+    }
+
 }
