119b251d9a02fc39f4d67a168ea633c11847629d
Paul Parrone <paul.parrone@gmail.com>
2022-08-10 17:28:11 -0400

    Changes and refactorings.
    Took 32 minutes

--- src/main/resources/archetype-resources/src/main/java/lush/controller/ExampleController.java
+++ src/main/resources/archetype-resources/src/main/java/lush/controller/ExampleController.java
@@ -1,6 +1,7 @@
 package ${package}.lush.controller;
 
 import com.px3j.lush.core.exception.LushException;
+import com.px3j.lush.core.exception.StackTraceToLoggerWriter;
 import com.px3j.lush.core.model.AnyModel;
 import com.px3j.lush.core.model.LushAdvice;
 import com.px3j.lush.core.model.LushContext;
@@ -17,6 +18,7 @@ import reactor.core.publisher.Mono;
 
 import ${package}.lush.model.Cat;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -121,6 +123,66 @@ public class ExampleController {
                 ));
     }
 
+    /**
+     * This endpoint illustrates how an endpoint may handle checked exceptions on its own.
+     *
+     * @param lushContext  The LushContext for this request.
+     * @return A simple key/value pair.
+     */
+    @LushControllerMethod
+    @GetMapping("endpointHandledException")
+    @PreAuthorize("isAuthenticated()")
+    public Mono<AnyModel> endpointHandledException( @Parameter(hidden = true) LushContext lushContext ) {
+        // Here, we'll use the LushAdvice to signify the failure to the consumer
+        //
+        LushAdvice advice = lushContext.getAdvice();
+
+        try {
+            methodThatThrowsIOException();
+            return Mono.just( AnyModel.from("prop1", "value1") );
+        }
+        catch (IOException e) {
+            // Since we want to handle this ourselves, we do so here
+
+            // First, log it in the Lush style
+            e.printStackTrace( new StackTraceToLoggerWriter(log) );
+
+            // Set an application status code in advice:
+            advice.setStatusCode( 600 );  // in this scenario the code can be set to something the consumer can understand
+
+            // Possibly add any extras that the consumer can use:
+            advice.putExtra( "message", "file not found" );  // again, this can be anything that the consumer can understand
+
+            return Mono.empty();
+        }
+    }
+
+    /**
+     * This endpoint illustrates how an endpoint may handle checked exceptions on its own.
+     *
+     * @param lushContext  The LushContext for this request.
+     * @return A simple key/value pair.
+     */
+    @LushControllerMethod
+    @GetMapping("endpointWrappedException")
+    @PreAuthorize("isAuthenticated()")
+    public Mono<AnyModel> endpointWrappedException( @Parameter(hidden = true) LushContext lushContext ) {
+        // Here, we'll use the LushAdvice to signify the failure to the consumer
+        //
+        LushAdvice advice = lushContext.getAdvice();
+
+        try {
+            methodThatThrowsIOException();
+            return Mono.just( AnyModel.from("prop1", "value1") );
+        }
+        catch (IOException e) {
+            // Our internal API throws a checked exception, Lush believes only in unchecked exceptions.  Endpoint can
+            // simply wrap it in a LushException and rethrow it.  The architecture will kick in and ensure that it is
+            // logged/propagated to the caller properly.
+            throw new LushException( "couldn't open that file", e );
+        }
+    }
+
 
     /**
      * This endpoint illustrates an unexpected exception that isn't wrapped by Lush. (notice, no @LushControllerMethod
@@ -179,5 +241,8 @@ public class ExampleController {
 
     private void methodThatThrowsUnexpectedException() {
         throw new LushException( "Illustrate Exception Handling" );
+    }
+    private void methodThatThrowsIOException() throws   IOException{
+        throw new IOException( "This is a illustrative checked exception" );
     }
 }
